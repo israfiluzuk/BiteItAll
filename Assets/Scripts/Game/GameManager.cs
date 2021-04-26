724bca3ad545f4c5bacd2727f579629974d47992
israfiluzuk <israfiluzuk@hotmail.com>
2021-04-26 09:12:06 +0300

    fruits, game over, last tower added

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : LocalSingleton<GameManager>
 {
+    [SerializeField] Text scoreText;
+    [SerializeField] Button buttonRestart;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = "0";
+        buttonRestart.gameObject.SetActive(false);
+        buttonRestart.onClick.AddListener(() => RestartGame());
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    internal void UpdadeScore(int score = 0, int multiplier = 1)
+    {
+        score *= multiplier;
+        scoreText.text = score.ToString();
+    }
+
+    internal void Restart()
+    {
+        buttonRestart.gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,46 +14,82 @@ public class Player : MonoBehaviour
     [SerializeField] float jumpForce;
     [SerializeField] float gravity;
     [SerializeField] GameObject explosionParticle;
+    [SerializeField] SkinnedMeshRenderer manHead;
     Vector3 vec;
+
+    Quaternion downRotation;
+    Quaternion upRotation;
+    bool isPlayerDown = false;
+    Vector3 lastPosition;
+    Vector3 currentPosition;
+    bool isFoodEaten = false;
+    private int playerScore = 0;
+    [SerializeField]private int multiplier = 1;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cameraHolder = Camera.main.transform.parent;
+        downRotation = Quaternion.Euler(-45, 0, 0);
+        upRotation = Quaternion.Euler(45, 0, 0);
+        MoveBSValueTo("A", 0, .18f);
+        MoveBSValueTo("M", 100, .18f);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider col)
     {
-        if (collision.collider.CompareTag("Food"))
+        if (col.gameObject.CompareTag("Food"))
         {
-            DestroyFood(collision.gameObject);
+            isFoodEaten = true;
+            DestroyFood(col.gameObject);
+            MoveBSValueTo("A", 58, .18f);
+            MoveBSValueTo("M", 0, .18f);
+            playerScore++;
+            GameManager.Instance.UpdadeScore(playerScore,multiplier);
         }
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            GameManager.Instance.Restart();
+        }
+
     }
 
     private void DestroyFood(GameObject gameObject)
     {
-        GameObject splashedObject =  Instantiate(explosionParticle, transform.position, Quaternion.identity) as GameObject;
-
+        GameObject splashedObject = Instantiate(explosionParticle, transform.position, Quaternion.identity) as GameObject;
+        splashedObject.transform.Rotate(-45, 90, 0);
         //Get the Particle System from the new GameObject.
         ParticleSystem PartSystem = splashedObject.GetComponent<ParticleSystem>();
         //Get the MainModule of the ParticleSystem
         ParticleSystem.MainModule ma = PartSystem.main;
         float totalDuration = PartSystem.duration + PartSystem.startLifetime;
+        //ma.startColor = Food.Instance.FoodColor;
         ma.startColor = gameObject.GetComponent<Renderer>().material.color;
 
         //Position the Particle System at the Balloons Location.
         splashedObject.transform.position = gameObject.transform.position;
 
         Destroy(gameObject);
-        Destroy(PartSystem.gameObject,totalDuration);
+        Destroy(PartSystem.gameObject, totalDuration);
     }
-
     private void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        currentPosition = transform.position;
+
+        if (Input.GetKeyUp(KeyCode.Space) || Input.touchCount > 0 || Input.GetMouseButtonDown(0))
         {
             isJump = true;
         }
+        if (lastPosition.y > currentPosition.y)
+        {
+            isPlayerDown = true;
+            transform.DORotate(new Vector3(30, 0, 0), .28f);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        lastPosition = currentPosition;
     }
 
     // Update is called once per frame
@@ -61,10 +98,13 @@ public class Player : MonoBehaviour
         rb.AddForce(Vector3.forward * playerSpeed * Time.fixedDeltaTime);
         if (isJump)
         {
+            rb.velocity = Vector3.zero;
+            DOTween.Kill(transform);
+            transform.DORotate(new Vector3(-30, 0, 0), .28f);
             rb.AddForce(Vector3.up * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
             isJump = false;
         }
-
+        //transform.DORotate(new Vector3(30, 0, 0), .2f);
         //rb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
     }
 
@@ -76,4 +116,23 @@ public class Player : MonoBehaviour
 
     //    cameraHolder.transform.position = vec;
     //}
+
+    public void MoveBSValueTo(string bsName, float value, float duration)
+    {
+        DOTween.To(() => manHead.GetBlendShapeWeight(GetBSIndexByName(manHead, bsName)), x => manHead.SetBlendShapeWeight(GetBSIndexByName(manHead, bsName), x), value, duration);
+
+    }
+
+    public static int GetBSIndexByName(SkinnedMeshRenderer faceMeshRenderer, string bsName)
+    {
+        Mesh m = faceMeshRenderer.sharedMesh;
+        for (int i = 0; i < m.blendShapeCount; i++)
+        {
+            if (m.GetBlendShapeName(i).Equals(bsName))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
